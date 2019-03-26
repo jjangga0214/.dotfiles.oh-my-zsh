c2c217728f28c69ed573be15f99943da85e36143
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-03-26 13:20:49 +0900

    split plugin.sh from install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@ BASE_DIR=$(dirname "$0")
 
 # oh-my-zsh 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-: "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}"
 
 # plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/djui/alias-tips $ZSH_CUSTOM/plugins/alias-tips
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+bash $BASE_DIR/plugin.sh
 
 # config
 bash $BASE_DIR/config.sh
---
+++ plugin.sh
@@ -0,0 +1,5 @@
+: "${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/djui/alias-tips $ZSH_CUSTOM/plugins/alias-tips
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
