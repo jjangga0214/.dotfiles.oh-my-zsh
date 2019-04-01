ddbf1314f0ce1e58d6701340c11b027a6cde9ee8
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-04-01 20:25:11 +0900

    add plugin dotenv; update install url

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 BASE_DIR=$(dirname "$0")
 
 # oh-my-zsh 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # plugins
 bash $BASE_DIR/plugin.sh
