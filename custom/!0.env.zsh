868cfcbd8906d654bd53f3430ce1f7a50affe8ea
GIL B. Chan <bnbcmindnpass@gmail.com>
2020-01-19 20:24:22 +0900

    fix $GOROOT

--- custom/!0.env.zsh
+++ custom/!0.env.zsh
@@ -20,7 +20,7 @@ export KUBE_PS1_SYMBOL_COLOR="yellow"
 ### Where go compiler would be considered installed. 
 ### /usr/local/go is the default. 
 ### This isn't needed if go is installed with snap
-# export GOROOT=/usr/local/go/bin
+export GOROOT=/usr/local/go
 ## GOPATH 
 ### Where go packages and projects will reside. 
 ### e.g. `go get <pkg>` installs <pkg> here.
